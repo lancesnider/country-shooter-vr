27ee1e36e5acf341e142d111fae44bdf96e7aefd
Lance <lance@lancesnider.com>
2019-07-20 12:13:46 -0700

    Improve bullet accuracy

--- Assets/_CountryShooter/Player/Bullet.cs
+++ Assets/_CountryShooter/Player/Bullet.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
   private Transform thisTransform;
+  private Rigidbody thisRigidbody;
 
   void Start()
   {
     thisTransform = transform;
+    thisRigidbody = GetComponent<Rigidbody>();
   }
 
   void Update()
   {
+    thisTransform.rotation = Quaternion.LookRotation(thisRigidbody.velocity);
     // Stop tracking the bullet after it gets far away
     if (thisTransform.position.y < -10)
     {
